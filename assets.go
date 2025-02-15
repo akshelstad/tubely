4b5724b61ac6123840a65473ebc1fde2f23899d0
akshelstad <akshelstad@gmail.com>
2025-02-15 13:45:15 -0700

    caching

--- assets.go
+++ assets.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +14,30 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+// getAssetPath generates a path for an asset based on the videoID and media type.
+// It uses the helper function mediaTypeToExt to retrieve the file extension.
+func getAssetPath(videoID uuid.UUID, mediaType string) string {
+	ext := mediaTypeToExt(mediaType)
+	return fmt.Sprintf("%s%s", videoID, ext)
+}
+
+// getAssetDiskPath generates the path on disk for an asset. It uses the asset path root defined in the configuration.
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
+	return filepath.Join(cfg.assetsRoot, assetPath)
+}
+
+// getAssetURL generates a URL for the asset.
+func (cfg apiConfig) getAssetURL(assetPath string) string {
+	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
+}
+
+// mediaTypeToExt is a helper function that extracts the file extension from the media type string.
+func mediaTypeToExt(mediaType string) string {
+	parts := strings.Split(mediaType, "/")
+	if len(parts) != 2 {
+		return ".bin"
+	}
+	return "." + parts[1]
 }
--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +31,70 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// maxMemory represents 10MB of memory by bitshifting the number 10 20 times.
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil { // Respond with a 400 code if the form file can not be parsed.
+		respondWithError(w, http.StatusBadRequest, "unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	// Get media type from `Content-Type` header.
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil { // Respond with a 500 code if media type can not be extracted from the Content-Type header.
+		respondWithError(w, http.StatusInternalServerError, "invalid Content-Type header", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		// Respond with a 400 code if media type is not a jpeg or png.
+		respondWithError(w, http.StatusBadRequest, "invalid file type: thumbnail should be a jpeg or png", nil)
+		return
+	}
+
+	// Get the asset path string and the actual asset path on disk.
+	assetPath := getAssetPath(videoID, mediaType)
+	assetDiskPath := cfg.getAssetDiskPath(assetPath)
+
+	// Create new file on disk.
+	newFile, err := os.Create(assetDiskPath)
+	if err != nil { // Respond with a 500 code if file can not be created.
+		respondWithError(w, http.StatusInternalServerError, "unable to create file on server", err)
+	}
+	defer newFile.Close()
+	// Copy contents from multipart file to new file.
+	if _, err = io.Copy(newFile, file); err != nil { // Respond with a 500 code if the file can not be copied to new file on disk.
+		respondWithError(w, http.StatusInternalServerError, "unable to copy file data to new file", err)
+		return
+	}
+
+	// Get the video metadata from database.
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil { // Respond with a 404 code if video can not be retrieved from database.
+		respondWithError(w, http.StatusNotFound, "unable to retrieve video metadata", err)
+		return
+	}
+	if userID != video.UserID { // Respond with a 401 code if user is not the video owner.
+		respondWithError(w, http.StatusUnauthorized, "user is not authorized to update this video", err)
+		return
+	}
+
+	// Generate URL to asset.
+	dataURL := cfg.getAssetURL(assetPath)
+
+	// Set the URL in the metadata.
+	video.ThumbnailURL = &dataURL
+
+	// Update the database record for the video to include the thumbnail information.
+	err = cfg.db.UpdateVideo(video)
+	if err != nil { // Respond with a 500 code if the video metadata can not be updated.
+		respondWithError(w, http.StatusInternalServerError, "unable to update video metadata in database", err)
+		return
+	}
+	// Respond with a 200 code and the metaData struct indicating success.
+	respondWithJSON(w, http.StatusOK, video)
 }
